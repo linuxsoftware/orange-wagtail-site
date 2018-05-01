c1ca29ea278ea9f10c26877244ec16fc78b1c02d
David <david@linuxsoftware.co.nz>
2018-05-02 09:55:31 +1200

    Bump joyous

--- demo/settings/dev.py
+++ demo/settings/dev.py
@@ -5,6 +5,9 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+from django.contrib.messages import constants as message_constants
+MESSAGE_LEVEL = message_constants.DEBUG
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '@tm_6wd$(17$5%5f%ci&+&_6(#*a9=24e2@mxbej4136&8r)$)'
 
