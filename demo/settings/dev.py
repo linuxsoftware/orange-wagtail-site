3fb558fd19bcc694f4d3b32f7ed78a3eb6f2701d
David <david@linuxsoftware.co.nz>
2018-05-12 21:16:26 +1200

    Joyous bump

--- demo/settings/dev.py
+++ demo/settings/dev.py
@@ -14,6 +14,42 @@ SECRET_KEY = '@tm_6wd$(17$5%5f%ci&+&_6(#*a9=24e2@mxbej4136&8r)$)'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+# LOGGING = {
+#     'version': 1,
+#
+#     'disable_existing_loggers': True,
+#     'formatters': {
+#         'verbose': {
+#             'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+#         },
+#         'simple': {
+#             'format': '%(levelname)s %(message)s'
+#         },
+#     },
+#     'handlers': {
+#         'null': {
+#             'level': 'DEBUG',
+#             'class': 'logging.NullHandler',
+#         },
+#         'console':{
+#             'level': 'DEBUG',
+#             'class': 'logging.StreamHandler',
+#             'formatter': 'simple'
+#         },
+#         'mail_admins': {
+#             'level': 'ERROR',
+#             'class': 'django.utils.log.AdminEmailHandler',
+#             'filters': [],
+#         }
+#     },
+#
+#     'loggers': {
+#         'django.db.backends': {
+#             'handlers': ['console'],
+#             'level': 'DEBUG',
+#         },
+#     },
+# }
 
 try:
     from .local import *
