6c5d849615c62328cd40929445f6dc56f7677594
David <david@linuxsoftware.co.nz>
2018-07-10 11:32:11 +1200

    debugging

--- events/models.py
+++ events/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from ls.joyous.models import CalendarPage
 
+#from ls.joyous.models import SpecificCalendarPage, GeneralCalendarPage
+#CalendarPage.is_creatable = True
+#SpecificCalendarPage.is_creatable = True
+#GeneralCalendarPage.is_creatable = True
+
 # Create a demo version of the Calendar
 CalendarPage.is_creatable = False
 
