e05527c5b2b69007f2cf770c0e60ea4c32442b6b
David <david@linuxsoftware.co.nz>
2019-05-20 23:23:35 +1200

    Add TagsDemoPage

--- events/models.py
+++ events/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from wagtail.core.models import Page
+from wagtail.core.fields import RichTextField
+from wagtail.admin.edit_handlers import FieldPanel
 from ls.joyous.models import CalendarPage
 
 #from ls.joyous.models import SpecificCalendarPage, GeneralCalendarPage
@@ -15,3 +18,15 @@ class DemoCalendarPage(CalendarPage):
         verbose_name = "Calendar Demo"
 
     parent_page_types = ['home.HomePage']
+
+
+class TagsDemoPage(Page):
+    welcome = RichTextField(default='', blank=True)
+    welcome.help_text = "A welcome message"
+    content = RichTextField(default='', blank=True)
+    content.help_text = "An area of text for whatever you like"
+
+    content_panels = Page.content_panels + [
+        FieldPanel('welcome', classname="full"),
+        FieldPanel('content', classname="full"),
+        ]
